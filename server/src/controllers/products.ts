a4e16a14877373b2f2df6a3275c2884491263d3a
patelbansari <bansupatel25@gmail.com>
2022-03-17 23:56:57 +0000

    feat: get all and product details api

--- server/src/controllers/products.ts
+++ server/src/controllers/products.ts
@@ -1,13 +1,43 @@
 import express from 'express'
+import { Constants } from '../util/constants'
+import logger from '../util/logger'
+import products from '../db/data/products.json'
 
 const router = express.Router()
 
 router.get('/all', (_, res) => {
-  res.json({})
+  try {
+    // return all products data
+    res.status(Constants.STATUS_CODE.OK).json(products)
+  } catch (error) {
+    // error handling and sending appropriate response
+    logger.error(error)
+    res
+      .status(Constants.STATUS_CODE.INTERNAL_SERVER_ERROR)
+      .json({ error: Constants.ERROR_MESSAGES.ERR_INTERNAL_SERVER_ERROR })
+  }
 })
 
-router.get('/:id', (_, res) => {
-  res.json({})
+router.get('/:id', (req, res) => {
+  // find by ID in products data
+  const findProduct = products.find((p) => p.id === req.params.id)
+  try {
+    // if found then return that data
+    if (findProduct) {
+      res.status(Constants.STATUS_CODE.OK).json(findProduct)
+    } else {
+      // sending appropriate response
+      res
+        .status(Constants.STATUS_CODE.BAD_REQUEST)
+        .json({ error: Constants.ERROR_MESSAGES.ERR_PRODUCT_NOT_FOUND })
+    }
+  } catch (error) {
+    // error handling and sending appropriate response
+    logger.error(error)
+    res
+      .status(Constants.STATUS_CODE.INTERNAL_SERVER_ERROR)
+      .json({ error: Constants.ERROR_MESSAGES.ERR_INTERNAL_SERVER_ERROR })
+  }
 })
 
 export default router
